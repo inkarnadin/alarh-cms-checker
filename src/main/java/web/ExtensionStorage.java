c0f33fd9fa659a648e8fe63471039e0eebfed49c
a.alekseev <a.alekseev@netris.ru>
2020-09-10 22:21:01 +0300

    feat: change read resources file && configure building

--- src/main/java/web/ExtensionStorage.java
+++ src/main/java/web/ExtensionStorage.java
@@ -3,9 +3,7 @@ package web;
 import lombok.Getter;
 import lombok.SneakyThrows;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
@@ -19,20 +17,23 @@ public class ExtensionStorage {
 
     @SneakyThrows
     public void feedJoomlaComponents() {
-        Path path = Paths.get("src/main/resources/joomla-ext.txt");
+        InputStream in = getClass().getResourceAsStream("/joomla-ext.txt");
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+            Stream<String> lines = reader.lines();
+            lines.forEach(str -> plugins.add(str));
+            lines.close();
+        }
 
-        Stream<String> lines = Files.lines(path);
-        lines.forEach(str -> components.add(str));
-        lines.close();
     }
 
     @SneakyThrows
     public void feedWordPressPlugins() {
-        Path path = Paths.get("src/main/resources/wordpress-ext.txt");
-
-        Stream<String> lines = Files.lines(path);
-        lines.forEach(str -> plugins.add(str));
-        lines.close();
+        InputStream in = getClass().getResourceAsStream("/wordpress-ext.txt");
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+            Stream<String> lines = reader.lines();
+            lines.forEach(str -> plugins.add(str));
+            lines.close();
+        }
     }
 
 }
--- src/main/java/web/Main.java
+++ src/main/java/web/Main.java
@@ -3,14 +3,48 @@ package web;
 import web.joomla.processor.JoomlaCheckComponentProcessor;
 import web.wordpress.processor.WordPressCheckPluginProcessor;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        //IProcessor processor = new JoomlaCheckComponentProcessor(args[0], args[1]);
-        //processor.process();
+        System.out.println("===========================================================");
+        System.out.println("Welcome! This is application for check installed plugins for CMS");
+        System.out.println("In current time it is support only Joomla CMS and WordPress");
+        System.out.println("===========================================================");
+        System.out.println("\n");
 
-        IProcessor processor = new WordPressCheckPluginProcessor(args[0], args[1]);
-        processor.process();
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                IProcessor processor;
+
+                System.out.print("Choose CMS type [Joomla|WP]: ");
+                String cms = scanner.nextLine();
+
+                System.out.print("Set protocol [http|https]: ");
+                String protocol = scanner.nextLine();
+
+                System.out.print("Set target host [example.com]: ");
+                String host = scanner.nextLine();
+
+                if ("Joomla".equalsIgnoreCase(cms)) {
+                    processor = new JoomlaCheckComponentProcessor(protocol, host);
+                } else if ( "WP".equalsIgnoreCase(cms)) {
+                    processor = new WordPressCheckPluginProcessor(protocol, host);
+                } else {
+                    throw new IllegalArgumentException("Unsupported CMS type");
+                }
+                processor.process();
+
+                System.out.println("===========================================================");
+                System.out.println("\n");
+                System.out.print("Try again? (y/n): ");
+                String answer = scanner.nextLine();
+
+                if ("n".equalsIgnoreCase(answer) || "no".equalsIgnoreCase(answer))
+                    System.exit(1);
+            }
+        }
     }
 
 }
