e25b6de4dc45f848c3c9e08923da76fb9b45a4b2
inkarnadin <nasyrovvad@gmail.com>
2020-11-25 02:51:25 +0300

    feat: add checking WP via main page generator tag

--- src/main/java/web/cms/wordpress/WordPressCheckProcessor.java
+++ src/main/java/web/cms/wordpress/WordPressCheckProcessor.java
@@ -4,30 +4,58 @@ import com.google.inject.Inject;
 import okhttp3.Response;
 import web.http.Host;
 import web.http.HttpValidator;
+import web.http.ResponseBodyHandler;
 import web.module.annotation.Get;
+import web.parser.TextParser;
 import web.struct.AbstractProcessor;
 import web.struct.Destination;
 import web.http.Request;
 
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.regex.Pattern;
 
 public class WordPressCheckProcessor extends AbstractProcessor {
 
     private final static String successMessage = "  * WordPress tags have been found!";
 
     private final Request request;
+    private final TextParser<Boolean> parser;
     private final Destination destination;
 
     @Inject
     WordPressCheckProcessor(@Get Request request,
+                            TextParser<Boolean> parser,
                             Destination destination) {
         this.request = request;
+        this.parser = parser;
         this.destination = destination;
     }
 
     @Override
     public void process() {
+        if (checkViaMainPage() || checkViaSpecifyPaths())
+        destination.insert(0, successMessage);
+    }
+
+    private boolean checkViaMainPage() {
+        Integer[] codes = { 200 };
+        Pattern pattern = Pattern.compile("<meta name=\"generator\" content=\"(WordPress).*/>");
+
+        Host host = new Host(protocol, server, null);
+        try (Response response = request.send(host)) {
+            Integer code = response.code();
+
+            if (Arrays.asList(codes).contains(code)) {
+                String body = ResponseBodyHandler.readBody(response);
+                parser.configure(pattern, 0);
+                return parser.parse(body);
+            }
+        }
+        return false;
+    }
+
+    private boolean checkViaSpecifyPaths() {
         String[] paths = { "wp-content", "wp-admin" };
         Integer[] codes = { 200, 403 };
 
@@ -38,11 +66,11 @@ public class WordPressCheckProcessor extends AbstractProcessor {
             try (Response response = request.send(host)) {
                 Integer code = response.code();
                 if (Arrays.asList(codes).contains(code) && !HttpValidator.isRedirect(response)) {
-                    destination.insert(0, successMessage);
-                    return;
+                    return true;
                 }
             }
         }
+        return false;
     }
 
     @Override
