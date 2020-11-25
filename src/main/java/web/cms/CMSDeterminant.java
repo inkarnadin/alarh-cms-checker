096e0cf649ac74a0ecb95d7003f645cfb807bf77
inkarnadin <nasyrovvad@gmail.com>
2020-11-25 09:39:39 +0300

    style: change to lambda style

--- src/main/java/web/cms/CMSDeterminant.java
+++ src/main/java/web/cms/CMSDeterminant.java
@@ -37,30 +37,22 @@ public class CMSDeterminant implements Determinant<CMSType, Destination> {
         wpCheckProcessor.configure(params.getProtocol(), params.getServer());
         wpCheckProcessor.process();
         Optional<Destination> wpTransmit = wpCheckProcessor.transmit();
-        wpTransmit.ifPresent(destination -> {
-            result.put(CMSType.WORDPRESS, destination);
-        });
+        wpTransmit.ifPresent(destination -> result.put(CMSType.WORDPRESS, destination));
 
         jmCheckProcessor.configure(params.getProtocol(), params.getServer());
         jmCheckProcessor.process();
         Optional<Destination> jmTransmit = jmCheckProcessor.transmit();
-        jmTransmit.ifPresent(destination -> {
-            result.put(CMSType.JOOMLA, destination);
-        });
+        jmTransmit.ifPresent(destination -> result.put(CMSType.JOOMLA, destination));
 
         yiiCheckProcessor.configure(params.getProtocol(), params.getServer());
         yiiCheckProcessor.process();
         Optional<Destination> yiiTransmit = yiiCheckProcessor.transmit();
-        yiiTransmit.ifPresent(destination -> {
-            result.put(CMSType.YII, destination);
-        });
+        yiiTransmit.ifPresent(destination -> result.put(CMSType.YII, destination));
 
         dleCheckProcessor.configure(params.getProtocol(), params.getServer());
         dleCheckProcessor.process();
         Optional<Destination> dleTransmit = dleCheckProcessor.transmit();
-        dleTransmit.ifPresent(destination -> {
-            result.put(CMSType.DATALIFE_ENGINE, destination);
-        });
+        dleTransmit.ifPresent(destination -> result.put(CMSType.DATALIFE_ENGINE, destination));
 
         return result;
     }
