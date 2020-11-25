ea3b9b289c15228407b9659da650ff8610302988
inkarnadin <nasyrovvad@gmail.com>
2020-11-25 17:15:02 +0300

    chore: correct version number

--- src/main/java/web/cms/datalife/DataLifeLogoMap.java
+++ src/main/java/web/cms/datalife/DataLifeLogoMap.java
@@ -17,7 +17,7 @@ public class DataLifeLogoMap {
         ClassLoader classLoader = this.getClass().getClassLoader();
 
         put(classLoader.getResource("datalife/4.jpg"), "4.0, 4.3, 4.5, 5.0, 5.1, 5.2, 5.3, 5.5, 5.7, 6.0, 6.2, 6.5, 6.7, 7.0, 7.2, 7.3, 7.5");
-        put(classLoader.getResource("datalife/8.2.jpg"), "8.2, 8.3");
+        put(classLoader.getResource("datalife/8.2.jpg"), "8.2");
         put(classLoader.getResource("datalife/8.3.jpg"), "8.3");
         put(classLoader.getResource("datalife/9.1.jpg"), "8.5, 9.1, 9.2, 9.4, 9.5, 9.6, 9.7, 9.8, 10.0, 10.1");
         put(classLoader.getResource("datalife/10.2.png"), "10.2, 10.3");
