a9e07196a6cbb8214718ca552efc43b2bf848fd7
inkarnadin <nasyrovvad@gmail.com>
2020-11-26 15:20:53 +0300

    fix: simplify & fix wrong behavior

--- src/main/java/web/cms/CMSChecker.java
+++ src/main/java/web/cms/CMSChecker.java
@@ -3,13 +3,9 @@ package web.cms;
 import com.google.inject.Inject;
 import web.struct.*;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class CMSChecker extends AbstractChecker {
 
     private final Determinant<CMSType, Destination> determinant;
-    private final Map<CMSType, Destination> types = new HashMap<>();
 
     @Inject
     CMSChecker(Determinant<CMSType, Destination> determinant) {
@@ -18,9 +14,7 @@ public class CMSChecker extends AbstractChecker {
 
     @Override
     public void check(Params params) {
-        types.putAll(determinant.define(params));
-
-        types.forEach((cmsType, destination) -> {
+        determinant.define(params).forEach((cmsType, destination) -> {
             System.out.println(destination.fetch().get(0));
 
             Connector connector = CMSFactory.getCMSConnector(cmsType);
