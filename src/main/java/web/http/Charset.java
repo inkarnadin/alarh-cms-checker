06e21d12fedb58f5d5bc878f15621cf0b5d626be
inkarnadin <nasyrovvad@gmail.com>
2020-11-29 22:19:54 +0300

    feat: add ISO charset

--- src/main/java/web/http/Charset.java
+++ src/main/java/web/http/Charset.java
@@ -8,13 +8,17 @@ public class Charset {
 
     public static final String UTF8 = "UTF-8";
     public static final String WIN1251 = "windows-1251";
+    public static final String ISO_8859_1 = "iso-8859-1";
 
     public static String defineCharset(String contentType) {
         if (Objects.nonNull(contentType)) {
             Pattern pattern = Pattern.compile(".*charset=(.*)", Pattern.CASE_INSENSITIVE);
             Matcher mather = pattern.matcher(contentType);
-            if (mather.find() && Objects.equals(mather.group(1), WIN1251))
-                return WIN1251;
+            if (mather.find())
+                if (Objects.equals(mather.group(1), WIN1251))
+                    return WIN1251;
+                if (Objects.equals(mather.group(1), WIN1251))
+                    return ISO_8859_1;
         }
         return UTF8;
     }
