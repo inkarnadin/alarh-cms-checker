2eda412746c07b58e9cf994b3d8a41fd743f75ae
inkarnadin <nasyrovvad@gmail.com>
2020-12-01 15:21:18 +0300

    feat: update prior during check CMS - filter low importance CMS

--- src/main/java/web/cms/CMSChecker.java
+++ src/main/java/web/cms/CMSChecker.java
@@ -3,6 +3,9 @@ package web.cms;
 import com.google.inject.Inject;
 import web.struct.*;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class CMSChecker extends AbstractChecker {
 
     private final Determinant<CMSType, Destination> determinant;
@@ -14,7 +17,15 @@ public class CMSChecker extends AbstractChecker {
 
     @Override
     public void check(Params params) {
-        determinant.define(params).forEach((cmsType, destination) -> {
+        Map<CMSType, Destination> determinate = sortByValue(determinant.define(params));
+
+        if (determinate.size() > 1) {
+            determinate = determinate.entrySet().stream()
+                    .filter(x -> x.getValue().getImportance().ordinal() > 1)
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        }
+
+        determinate.forEach((cmsType, destination) -> {
             System.out.println(destination.fetch().get(0) + " => " + destination.getImportance());
 
             Connector connector = CMSFactory.getCMSConnector(cmsType);
@@ -25,6 +36,20 @@ public class CMSChecker extends AbstractChecker {
 
             System.out.println();
         });
+    }
+
+    public static Map<CMSType, Destination> sortByValue(Map<CMSType, Destination> map) {
+        List<Map.Entry<CMSType, Destination>> list = new ArrayList<>(map.entrySet());
+        list.sort(new Comparator<Map.Entry<CMSType, Destination>>() {
+            @Override
+            public int compare(Map.Entry<CMSType, Destination> o1, Map.Entry<CMSType, Destination> o2) {
+                if (o1.getValue().getImportance().ordinal() > o2.getValue().getImportance().ordinal())
+                    return 1;
+                return 0;
+            }
+        });
+
+        return list.stream().collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
 }
