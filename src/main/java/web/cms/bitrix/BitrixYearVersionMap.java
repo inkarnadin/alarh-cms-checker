6b4c84aae7d5f16134ab69677b7280aa7285de97
inkarnadin <nasyrovvad@gmail.com>
2020-12-04 11:29:38 +0300

    feat: calibrate checking Bitrix version

--- src/main/java/web/cms/bitrix/BitrixYearVersionMap.java
+++ src/main/java/web/cms/bitrix/BitrixYearVersionMap.java
@@ -10,20 +10,20 @@ public class BitrixYearVersionMap implements VersionMap {
     Map<Long, String> versionMap = new HashMap<>();
 
     BitrixYearVersionMap() {
-        versionMap.put(2007L, "6.0.0");
-        versionMap.put(2008L, "6.0.0, 7.0.0");
-        versionMap.put(2009L, "8.0.0");
-        versionMap.put(2010L, "9.0.0");
-        versionMap.put(2011L, "10.0.0, 11.0.0");
-        versionMap.put(2012L, "11.0.0, 12.0.0");
-        versionMap.put(2013L, "12.0.0, 14.0.0");
-        versionMap.put(2014L, "14.0.0");
-        versionMap.put(2015L, "14.0.0, 15.0.0");
-        versionMap.put(2016L, "15.0.0, 16.0.0");
-        versionMap.put(2017L, "15.0.0, 16.0.0, 17.0.0, 18.0.0");
-        versionMap.put(2018L, "17.0.0, 18.0.0");
-        versionMap.put(2019L, "18.0.0, 19.0.0, 20.0.0");
-        versionMap.put(2020L, "20.0.0");
+        versionMap.put(2007L, "6.0.0+");
+        versionMap.put(2008L, "6.0.0,+");
+        versionMap.put(2009L, "8.0.0+");
+        versionMap.put(2010L, "9.0.0+");
+        versionMap.put(2011L, "10.0.0+");
+        versionMap.put(2012L, "11.0.0+");
+        versionMap.put(2013L, "12.0.0+");
+        versionMap.put(2014L, "14.0.0+");
+        versionMap.put(2015L, "14.0.0+");
+        versionMap.put(2016L, "15.0.0+");
+        versionMap.put(2017L, "15.0.0+");
+        versionMap.put(2018L, "17.0.0+");
+        versionMap.put(2019L, "18.0.0+");
+        versionMap.put(2020L, "20.0.0+");
     }
 
     @Override
