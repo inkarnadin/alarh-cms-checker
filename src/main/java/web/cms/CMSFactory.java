046b9cd79935ab0cd2ef382cbfae59c22322c815
inkarnadin <nasyrovvad@gmail.com>
2020-12-05 10:31:58 +0300

    refactor: simplify cms factory

--- src/main/java/web/cms/CMSFactory.java
+++ src/main/java/web/cms/CMSFactory.java
@@ -3,56 +3,13 @@ package web.cms;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import web.module.*;
-import web.module.provider.*;
 import web.struct.Connector;
 
 public class CMSFactory {
 
     public static Connector getCMSConnector(CMSType cmsType) {
-        Injector injector = Guice.createInjector(
-                new CMSModule()
-        );
-
-        switch (cmsType) {
-            case JOOMLA:
-                return injector.getInstance(JoomlaProvider.class).get();
-            case WORDPRESS:
-                return injector.getInstance(WordPressProvider.class).get();
-            case YII:
-                return injector.getInstance(YiiProvider.class).get();
-            case DATALIFE_ENGINE:
-                return injector.getInstance(DataLifeProvider.class).get();
-            case MAXSITE_CMS:
-                return injector.getInstance(MaxSiteProvider.class).get();
-            case DRUPAL:
-                return injector.getInstance(DrupalProvider.class).get();
-            case BITRIX:
-                return injector.getInstance(BitrixProvider.class).get();
-            case MODX:
-                return injector.getInstance(ModXProvider.class).get();
-            case LAVAREL:
-                return injector.getInstance(LavarelProvider.class).get();
-            case TILDA:
-                return injector.getInstance(TildaProvider.class).get();
-            case VAM_SHOP:
-                return injector.getInstance(VamShopProvider.class).get();
-            case NUXT_JS:
-                return injector.getInstance(NuxtProvider.class).get();
-            case MAGENTO:
-                return injector.getInstance(MagentoProvider.class).get();
-            case OPENCART:
-                return injector.getInstance(OpenCartProvider.class).get();
-            case INSALES:
-                return injector.getInstance(InSalesProvider.class).get();
-            case VIGBO:
-                return injector.getInstance(VigboProvider.class).get();
-            case RUBY_ON_RAILS:
-                return injector.getInstance(RubyOnRailsProvider.class).get();
-            case VUE_JS:
-                return injector.getInstance(VueProvider.class).get();
-            default:
-                throw new IllegalArgumentException("Unsupported CMS type");
-        }
+        Injector injector = Guice.createInjector(new CMSModule());
+        return injector.getInstance(cmsType.getProvider()).get();
     }
 
 }
--- src/main/java/web/cms/CMSType.java
+++ src/main/java/web/cms/CMSType.java
@@ -1,36 +1,38 @@
 package web.cms;
 
+import com.google.inject.Provider;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import web.module.provider.*;
+import web.struct.Connector;
 
 @Getter
 @RequiredArgsConstructor
 public enum CMSType {
 
-    WORDPRESS(0, "WordPress"),
-    JOOMLA(1, "Joomla!"),
-    DATALIFE_ENGINE(2, "DataLife Engine"),
-    MAXSITE_CMS(3, "MaxSite CMS"),
-    DRUPAL(4, "Drupal"),
-    BITRIX(5, "1C-Bitrix"),
-    MODX(6, "MODx"),
-    VAM_SHOP(7, "VamShop"),
-    MAGENTO(8, "Magento"),
-    OPENCART(9, "OpenCart"),
+    WORDPRESS               (0, "WordPress", WordPressProvider.class),
+    JOOMLA                  (1, "Joomla!", JoomlaProvider.class),
+    DATALIFE_ENGINE         (2, "DataLife Engine", DataLifeProvider.class),
+    MAXSITE_CMS             (3, "MaxSite CMS", MaxSiteProvider.class),
+    DRUPAL                  (4, "Drupal", DrupalProvider.class),
+    BITRIX                  (5, "1C-Bitrix", BitrixProvider.class),
+    MODX                    (6, "MODx", ModXProvider.class),
+    VAM_SHOP                (7, "VamShop", VamShopProvider.class),
+    MAGENTO                 (8, "Magento", MagentoProvider.class),
+    OPENCART                (9, "OpenCart", OpenCartProvider.class),
 
-    LAVAREL(10, "Lavarel"),
-    YII(11, "Yii Framework"),
-    NUXT_JS(12, "Nuxt.js"),
-    VUE_JS(13, "Vue.js"),
-    RUBY_ON_RAILS(14, "Ruby on Rails"),
+    LAVAREL                 (10, "Lavarel", LavarelProvider.class),
+    YII                     (11, "Yii Framework", YiiProvider.class),
+    NUXT_JS                 (12, "Nuxt.js", NuxtProvider.class),
+    VUE_JS                  (13, "Vue.js", VueProvider.class),
+    RUBY_ON_RAILS           (14, "Ruby on Rails", RubyOnRailsProvider.class),
 
-    TILDA(15, "Tilda"),
-    VIGBO(16, "Vigbo CMS"),
-    INSALES(17, "InSales"),
-
-    UNKNOWN(-1, "Unknown");
+    TILDA                   (15, "Tilda", TildaProvider.class),
+    VIGBO                   (16, "Vigbo CMS", VigboProvider.class),
+    INSALES                 (17, "InSales", InSalesProvider.class);
 
     private final int id;
     private final String name;
+    private final Class<? extends Provider<Connector>> provider;
 
 }
