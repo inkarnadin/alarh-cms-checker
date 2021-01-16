3d4209bddc6afdc052718a5297d1fce49a93a13c
inkarnadin <nasyrovvad@gmail.com>
2021-01-16 06:17:39 +0300

    feat: simplify

--- src/main/java/web/http/ResponseBodyHandler.java
+++ src/main/java/web/http/ResponseBodyHandler.java
@@ -4,32 +4,14 @@ import lombok.SneakyThrows;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.Reader;
 import java.util.Objects;
-
-import static web.http.Charset.*;
-import static web.http.Headers.CONTENT_TYPE;
 
 public class ResponseBodyHandler {
 
     @SneakyThrows
     synchronized public static String readBody(Response response) {
-        String charset = defineCharset(response.header(CONTENT_TYPE));
-
         ResponseBody body = response.body();
-        StringBuilder textBuilder = new StringBuilder();
-        try (Reader reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(body).byteStream(), charset))) {
-            try {
-                int c = 0;
-                while ((c = reader.read()) != -1)
-                    textBuilder.append((char) c);
-            } catch (Exception e) {
-                System.out.println(e.getMessage() + ": " + body.toString());
-            }
-        }
-        return textBuilder.toString();
+        return (Objects.nonNull(body)) ? body.string() : "";
     }
 
 }
