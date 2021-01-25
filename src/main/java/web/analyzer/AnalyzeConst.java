2ad3cb9e6ac57cc6adf714ddf4e23ad8d84e7d2c
inkarnadin <nasyrovvad@gmail.com>
2021-01-25 12:57:14 +0300

    feat: add content types const

--- src/main/java/web/analyzer/AnalyzeConst.java
+++ src/main/java/web/analyzer/AnalyzeConst.java
@@ -1,5 +1,7 @@
 package web.analyzer;
 
+import static web.http.ContentType.*;
+
 public class AnalyzeConst {
 
     public static final String[] BASE_PATH = new String[] { "" };
@@ -7,5 +9,8 @@ public class AnalyzeConst {
     public static final Integer[] SUCCESS_CODES = new Integer[] { 200, 304 };
     public static final Integer[] DENIED_CODES = new Integer[] { 401, 403 };
     public static final Integer[] ACCEPT_CODES = new Integer[] { 200, 304, 401, 403 };
+
+    public static final String[] SCRIPTS = new String[] { APPLICATION_JAVASCRIPT, APPLICATION_X_JAVASCRIPT };
+    public static final String[] IMAGES = new String[] { IMAGE_PNG, IMAGE_JPG };
 
 }
