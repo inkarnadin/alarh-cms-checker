165b75b890820d8014a901aef552a8e6019f09f3
inkarnadin <nasyrovvad@gmail.com>
2021-02-11 16:13:27 +0300

    feat: allow invalid cert

--- src/main/java/web/http/HttpClient.java
+++ src/main/java/web/http/HttpClient.java
@@ -4,17 +4,22 @@ import lombok.SneakyThrows;
 import okhttp3.Request;
 import okhttp3.*;
 
+import javax.net.ssl.*;
 import java.io.File;
+import java.security.SecureRandom;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 public class HttpClient implements Client {
 
+    private final WildCardTrustManager trustManager = new WildCardTrustManager();
     private final OkHttpClient okClient;
 
     public HttpClient() {
         okClient = new OkHttpClient().newBuilder()
+                .sslSocketFactory(provideSSLFactory(), trustManager)
                 .addNetworkInterceptor(provideCacheInterceptor())
+                .hostnameVerifier(provideVerifier())
                 .cache(new Cache(new File("http_cache"), 50L * 1024L * 1024L))                
                 .followSslRedirects(true)
                 .build();
@@ -46,6 +51,17 @@ public class HttpClient implements Client {
                     .header("Cache-Control", cacheControl.toString())
                     .build();
         };
+    }
+
+    @SneakyThrows
+    private SSLSocketFactory provideSSLFactory() {
+        SSLContext sslContext = SSLContext.getInstance("SSL");
+        sslContext.init(null, new TrustManager[] { trustManager }, new SecureRandom());
+        return sslContext.getSocketFactory();
+    }
+
+    private HostnameVerifier provideVerifier() {
+        return (hostname, session) -> true;
     }
 
 }
---
+++ src/main/java/web/http/WildCardTrustManager.java
@@ -0,0 +1,19 @@
+package web.http;
+
+import javax.net.ssl.X509TrustManager;
+import java.security.cert.X509Certificate;
+
+public class WildCardTrustManager implements X509TrustManager {
+
+    @Override
+    public void checkClientTrusted(X509Certificate[] chain, String authType) {}
+
+    @Override
+    public void checkServerTrusted(X509Certificate[] chain, String authType) {}
+
+    @Override
+    public X509Certificate[] getAcceptedIssuers() {
+        return new X509Certificate[0];
+    }
+
+}
