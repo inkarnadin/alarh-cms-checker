d94eeec33dd3aae927cdbd7265085b91666f441b
inkarnadin <nasyrovvad@gmail.com>
2023-04-29 01:25:36 +0300

    refactor: minor structure changes

--- src/main/java/web/cms/AbstractCMSProcessor.java
+++ src/main/java/web/cms/AbstractCMSProcessor.java
@@ -6,32 +6,48 @@ import web.struct.Destination;
 import web.struct.Processor;
 import web.struct.assignment.DefaultAssigner;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 
+/**
+ * Базовый абстрактный процессор для анализа данных.
+ *
+ * @author inkarnadin
+ * on 20-11-2020
+ */
 public abstract class AbstractCMSProcessor implements Processor<CMSType>, DefaultAssigner {
-
-    protected final Map<String, Integer> errorMap = new HashMap<>();
 
     protected String protocol;
     protected String server;
     protected Host host;
 
+    /**
+     * Метод предварительной настройки процессора.
+     *
+     * @param protocol протокол взаимодействия
+     * @param server адрес сервера
+     */
     @Override
     public void configure(String protocol, String server) {
-        Objects.requireNonNull(protocol, "Empty protocol value!");
-        Objects.requireNonNull(server, "Empty url value!");
+        Objects.requireNonNull(protocol, "Empty protocol value not allowed");
+        Objects.requireNonNull(server, "Empty url value not allowed");
 
         this.protocol = protocol;
         this.server = server;
         this.host = new Host(protocol, server);
     }
 
+    /**
+     * Метод обработки признаков определения.
+     */
     @Override
     public void process() {}
 
+    /**
+     * Метод интерпретации и передачи результата анализа результата.
+     *
+     * @return результат анализа
+     */
     @Override
     public Pair<CMSType, Optional<Destination>> transmit() {
         return null;
